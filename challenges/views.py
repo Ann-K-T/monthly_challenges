144e57bb70a763070865773c83942e3df435e81a
Ann Thomas <ann-thomas2010@hotmail.com>
2025-06-11 12:17:27 -0400

    send 404.html with render_to_string

--- challenges/views.py
+++ challenges/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
+from django.template.loader import render_to_string
 
 
 # dictionary to hold monthly challenges
@@ -52,4 +53,5 @@ def monthly_challenge(request, month):
             "text": challenge_text, "month_name": month
         })
     except:
-        return HttpResponseNotFound("<h1>This is not a valid month </h1>")
+        response_data = render_to_string("404.html")
+        return HttpResponseNotFound(response_data)
