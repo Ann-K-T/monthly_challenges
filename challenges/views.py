fda7781a7080f45884006374d0933c549528596b
Ann Thomas <ann-thomas2010@hotmail.com>
2025-06-11 09:35:04 -0400

    render text with {{}} in html doc

--- challenges/views.py
+++ challenges/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
-from django.template.loader import render_to_string
+# from django.template.loader import render_to_string
 
 
 # dictionary to hold monthly challenges
@@ -53,6 +53,7 @@ def monthly_challenge_by_number(request, month):
 def monthly_challenge(request, month):
     try:
         challenge_text = monthly_challenges[month]
-        return render(request, "challenges/challenge.html")
+        return render(request, "challenges/challenge.html", {
+            "text": challenge_text, "month_name": month.capitalize()})
     except:
         return HttpResponseNotFound("<h1>This is not a valid month </h1>")
